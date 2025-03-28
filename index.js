0d682fb34421681cb5fc79e2c83fbf2bd5e3ec5f
SimoMosca1997 <simonmosnet@gmail.com>
2025-03-28 12:08:10 +0100

    Aggiunto file HTML e aggiornato server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Servire il file HTML
 app.get("/", (req, res) => {
-  res.send("Server attivo!");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
